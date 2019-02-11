8edc985bc34d0cd1ab745ae54f0a69882da1a2ef
nolleh7707 <nolleh7707@gmail.com>
2019-02-11 12:40:29 +0900

    add functionality for generating wast.hpp

--- CMakeModules/wasm.cmake
+++ CMakeModules/wasm.cmake
@@ -2,39 +2,37 @@
 
 macro(add_unittest_header_files TARGET DESTINATION_FOLDER)
     set (target ${TARGET})
-    
-    # add_custom_command(OUTPUT ${DESTINATION_FOLDER}/${target}.wasm
-    #     DEPENDS ${target}.wast
-    #     COMMAND $<TARGET_FILE:eosio-wast2wasm> ${DESTINATION_FOLDER}/${target}.wast ${DESTINATION_FOLDER}/${target}.wasm -n
-    #     COMMENT "Generating WASM ${target}.wasm"
-    #     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-    #     VERBATIM
-    # )
-    # set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES ${target}.wasm)
 
-    # add_custom_command(OUTPUT ${DESTINATION_FOLDER}/${target}.wast.hpp
-    #     DEPENDS ${DESTINATION_FOLDER}/${target}.wast
-    #     COMMAND echo "const char* const ${TARGET_VARIABLE}_wast = R\"=====("  > ${DESTINATION_FOLDER}/${target}.wast.hpp
-    #     COMMAND cat ${DESTINATION_FOLDER}/${target}.wast >> ${DESTINATION_FOLDER}/${target}.wast.hpp
-    #     COMMAND echo ")=====\";"  >> ${DESTINATION_FOLDER}/${target}.wast.hpp
-    #     COMMENT "Generating ${target}.wast.hpp"
-    #     VERBATIM
-    # )
+    add_custom_command(TARGET ${TARGET}.wasm
+        POST_BUILD
+        DEPENDS ${target}.wasm
+        COMMAND eosio-wasm2wast ${DESTINATION_FOLDER}/${target}.wasm > ${DESTINATION_FOLDER}/${target}.wast
+        COMMENT "Generating WAST ${target}.wast"
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+        VERBATIM
+    )
+    #set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES ${target}.wasm)
 
-    if (EXISTS ${DESTINATION_FOLDER}/${target}.abi)
-        add_custom_command(TARGET ${TARGET}.wasm
-            POST_BUILD
-            DEPENDS ${DESTINATION_FOLDER}/${target}.abi
-            COMMAND echo "const char* const ${TARGET_VARIABLE}_abi = R\"=====("  > ${DESTINATION_FOLDER}/${target}.abi.hpp
-            COMMAND cat ${DESTINATION_FOLDER}/${target}.abi >> ${DESTINATION_FOLDER}/${target}.abi.hpp
-            COMMAND echo ")=====\";"  >> ${DESTINATION_FOLDER}/${target}.abi.hpp
-            COMMENT "Generating ${target}.abi.hpp"
-            VERBATIM
-        )
-        set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES ${target}.abi.hpp)
-        set(extra_target_dependency   ${DESTINATION_FOLDER}/${target}.abi.hpp)
-    else()
-        message("failed to find abi files in " ${DESTINATION_FOLDER}/${target}.abi)
-    endif()
+    add_custom_command(TARGET ${TARGET}.wasm
+        POST_BUILD
+        DEPENDS ${DESTINATION_FOLDER}/${target}.wast
+        COMMAND echo "const char* const ${TARGET_VARIABLE}_wast = R\"=====("  > ${DESTINATION_FOLDER}/${target}.wast.hpp
+        COMMAND cat ${DESTINATION_FOLDER}/${target}.wast >> ${DESTINATION_FOLDER}/${target}.wast.hpp
+        COMMAND echo ")=====\";"  >> ${DESTINATION_FOLDER}/${target}.wast.hpp
+        COMMENT "Generating ${target}.wast.hpp"
+        VERBATIM
+    )
+
+    add_custom_command(TARGET ${TARGET}.wasm
+        POST_BUILD
+        DEPENDS ${DESTINATION_FOLDER}/${target}.abi
+        COMMAND echo "const char* const ${TARGET_VARIABLE}_abi = R\"=====("  > ${DESTINATION_FOLDER}/${target}.abi.hpp
+        COMMAND cat ${DESTINATION_FOLDER}/${target}.abi >> ${DESTINATION_FOLDER}/${target}.abi.hpp
+        COMMAND echo ")=====\";"  >> ${DESTINATION_FOLDER}/${target}.abi.hpp
+        COMMENT "Generating ${target}.abi.hpp"
+        VERBATIM
+    )
+    #set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES ${target}.abi.hpp)
+    #set(extra_target_dependency   ${DESTINATION_FOLDER}/${target}.abi.hpp)
 
 endmacro(add_unittest_header_files)
--- contracts/eosio.token/eosio.token.abi.hpp
+++ contracts/eosio.token/eosio.token.abi.hpp
@@ -1,12 +1,1 @@
 const char* const _abi = R"=====(
-{
-    "____comment": "This file was generated with eosio-abigen. DO NOT EDIT Fri Feb  8 19:15:15 2019",
-    "version": "eosio::abi/1.1",
-    "structs": [],
-    "types": [],
-    "actions": [],
-    "tables": [],
-    "ricardian_clauses": [],
-    "variants": [],
-    "abi_extensions": []
-})=====";
