4d346c7556132dc4270c34f3e18fb72b38722bd3
nolleh7707 <nolleh7707@gmail.com>
2019-02-15 18:53:47 +0900

    coverage test, but cannot parse wast. what's the problem?

--- tests/eosio.token_tests.cpp
+++ tests/eosio.token_tests.cpp
@@ -1,11 +1,11 @@
-//#define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE Main
-
+#define NON_VALIDATING_TEST
 #include <boost/test/unit_test.hpp>
 #include <eosio/testing/tester.hpp>
+#include <eosio/chain/wast_to_wasm.hpp>
 
-#include "eosio.token/eosio.token.wast.hpp"
-#include "eosio.token/eosio.token.abi.hpp"
+#include "eosio.token2/eosio.token2.wast.hpp"
+#include "eosio.token2/eosio.token2.abi.hpp"
 
 #ifdef NON_VALIDATING_TEST
 #define TESTER tester
@@ -23,13 +23,14 @@ BOOST_AUTO_TEST_SUITE(foo)
 BOOST_AUTO_TEST_CASE(eosio_token_test) try {
     TESTER chain;
 
-    chain.create_account(N("acc_token"));
+    chain.create_account(N(eosio.token));
     chain.produce_blocks(10);
     
-    chain.set_code(N("eosio.token"), eosio_token_wast);
-    chain.set_abi(N("eosio.token"), eosio_token_abi);
+    chain.set_code(N(eosio.token), eosio_token2_wast);
+    // chain.set_code(N(eosio.token), wast_to_wasm(eosio_token_wast));
+    chain.set_abi(N(eosio.token), eosio_token2_abi);
 
-
+    BOOST_REQUIRE_EQUAL(1, 0);
 } FC_LOG_AND_RETHROW()
 
 BOOST_AUTO_TEST_SUITE_END()
